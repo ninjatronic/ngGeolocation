cb00b0a6dba946b5592eb8fb281e0da91ffaeb7d
Arsalan Ahmad <arsalanahmad1993@gmail.com>
2014-10-16 15:40:57 +0500

    update position object in getCurrentPosition
    update position object when the position is gotten for the first time

--- ngGeolocation.js
+++ ngGeolocation.js
@@ -15,6 +15,8 @@ angular
                     $window.navigator.geolocation.getCurrentPosition(
                         function(position) {
                             $rootScope.$apply(function() {
+                                retVal.position.coords = position.coords;
+                                retVal.position.timestamp = position.timestamp;
                                 deferred.resolve(position);
                             });
                         },
